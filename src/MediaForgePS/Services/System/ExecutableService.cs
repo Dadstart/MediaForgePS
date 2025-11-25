b7cf8620337321412434bb0b348d0df467ad81ab
Andrew Bishop <Dadstart@users.noreply.github.com>
2025-11-25 11:09:05 -0600

    Create executable class for running process

--- src/MediaForgePS/Services/System/ExecutableService.cs
+++ src/MediaForgePS/Services/System/ExecutableService.cs
@@ -1,11 +1,42 @@
 using System;
+using System.Diagnostics;
 
 namespace Dadstart.Labs.MediaForge.Services.System;
 
 public class ExecutableService : IExecutableService
 {
-    public string Execute(string command, string arguments)
+    /// <inheritdoc />
+    public async Task<ExecutableResult> Execute(string command, IEnumerable<string> arguments)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var processStartInfo = new ProcessStartInfo()
+            {
+                FileName = command,
+                Arguments = arguments.ToQuotedArgumentString(),
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using var process = new Process() { StartInfo = processStartInfo };
+
+            if (!process.Start())
+                throw new InvalidOperationException($"Failed to start process {command} with arguments {string.Join(" ", arguments)}");
+
+            // read both streams asynchronously to prevent deadlocks
+            var stdoutTask = process.StandardOutput.ReadToEndAsync();
+            var stderrTask = process.StandardError.ReadToEndAsync();
+
+            await process.WaitForExitAsync();
+
+            return new ExecutableResult(await stdoutTask, await stderrTask, process.ExitCode);
+
+        }
+        catch (Exception exc)
+        {
+            return new ExecutableResult(null, null, null, exc);
+        }
     }
 }
--- src/MediaForgePS/Services/System/IExecutableService.cs
+++ src/MediaForgePS/Services/System/IExecutableService.cs
@@ -2,5 +2,11 @@ namespace Dadstart.Labs.MediaForge.Services.System;
 
 public interface IExecutableService
 {
-    string Execute(string command, string arguments);
+    /// <summary>
+    /// Executes a command with the given arguments and returns the result.
+    /// </summary>
+    /// <param name="command">The command to execute.</param>
+    /// <param name="arguments">The arguments to pass to the command.</param>
+    /// <returns>The result of the command execution.</returns>
+    Task<ExecutableResult> Execute(string command, IEnumerable<string> arguments);
 }
