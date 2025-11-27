e7c39e648dcdefaffb3ef9e765a8fd03357aba5a
Andrew Bishop <Dadstart@users.noreply.github.com>
2025-11-27 09:11:32 -0600

    Ensure only the PowerShell logger provider is used.

--- src/MediaForgePS/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/MediaForgePS/DependencyInjection/ServiceCollectionExtensions.cs
@@ -28,31 +28,39 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddMediaForgeLogging(this IServiceCollection services)
     {
         services.AddSingleton<IPowerShellCommandContextAccessor, PowerShellCommandContext>();
+        
+        // Add logging infrastructure first - this enables generic logger resolution (ILogger<T>)
+        // This must be called before registering custom providers to ensure proper setup
+        services.AddLogging(builder =>
+        {
+            builder.SetMinimumLevel(LogLevel.Trace);
+        });
+        
+        // Register the PowerShell logger provider
+        // The default factory from AddLogging will automatically pick up all registered ILoggerProvider instances
         services.AddSingleton<ILoggerProvider>(sp =>
         {
             var contextAccessor = sp.GetRequiredService<IPowerShellCommandContextAccessor>();
             return new PowerShellLoggerProvider(contextAccessor);
         });
+        
+        // Override the factory to ensure only our PowerShell logger provider is used
+        // This factory will be used for creating loggers and supports generic logger resolution
         services.AddSingleton<ILoggerFactory>(sp =>
         {
-            var providers = sp.GetServices<ILoggerProvider>().ToList();
-            return LoggerFactory.Create(builder =>
+            var provider = sp.GetRequiredService<ILoggerProvider>();
+            return LoggerFactory.Create(factoryBuilder =>
             {
-                builder.ClearProviders();
-                foreach (var provider in providers)
-                {
-                    builder.AddProvider(provider);
-                }
-                builder.SetMinimumLevel(LogLevel.Trace);
+                factoryBuilder.ClearProviders();
+                factoryBuilder.AddProvider(provider);
+                factoryBuilder.SetMinimumLevel(LogLevel.Trace);
             });
         });
-        services.AddLogging(builder =>
-        {
-            builder.SetMinimumLevel(LogLevel.Trace);
-        });
+        
         services.AddSingleton<IDebuggerService, DebuggerService>();
 
         return services;
     }
 }
+
 
