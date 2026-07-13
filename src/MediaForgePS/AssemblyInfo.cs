4a6ded7f6b23cd90b311a12649987975e52da663
Andrew Bishop <andrew@dadstart.com>
2026-07-13 14:58:17 -0500

    Update version numbers to 0.12.0 in AssemblyInfo.cs and MediaForgePS.psd1
    - Bumped the assembly and module version numbers to 0.12.0 to reflect recent changes and improvements in the MediaForgePS project.

--- src/MediaForgePS/AssemblyInfo.cs
+++ src/MediaForgePS/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Dadstart LLC")]
 [assembly: AssemblyProduct("MediaForgePS")]
 [assembly: AssemblyCopyright("(c) Dadstart. All rights reserved.")]
-[assembly: AssemblyVersion("0.11.0")]
-[assembly: AssemblyFileVersion("0.11.0")]
-[assembly: AssemblyInformationalVersion("0.11.0")]
+[assembly: AssemblyVersion("0.12.0")]
+[assembly: AssemblyFileVersion("0.12.0")]
+[assembly: AssemblyInformationalVersion("0.12.0")]
 [assembly: Guid("a1b2c3d4-e5f6-4a5b-8c9d-0e1f2a3b4c5d")]
 
